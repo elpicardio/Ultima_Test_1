eb08653d5f1f43608ec4ccbfb5d7276e875ea4cd
Chris Kay <chris.kay00@icloud.com>
2022-11-27 18:08:26 +0000

    Initial Commit

---
+++ Ultima_Test/Ultima_TestApp.swift
@@ -0,0 +1,20 @@
+//
+//  Ultima_TestApp.swift
+//  Ultima_Test
+//
+//  Created by Chris Kay on 27/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Ultima_TestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
